205af5c900f46bdb97c3b5ed3c39f6beeff1f9eb
Mike-kwaku <30466141+Mike-kwaku@users.noreply.github.com>
2024-08-01 11:44:36 -0700

    Update ld.py

--- ld.py
+++ ld.py
@@ -1,17 +1,80 @@
 import pandas as pd
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+import s3fs
+df = pd.read_json("books.csv", orient="records", lines=True)
+df  = pd.read_json('spaceCompanies.json')
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+import cohere  
+co_client = cohere.Client(cohere_api_key, client_name='mongodb')
+# create a dataset in Cohere Platform
+dataset = co_client.create_dataset(name='companies',
+                                   data=open('spaceCompanies','r'),
+                                   keep_fields=["overview","title","year"],
+                                   dataset_type="embed-input").wait()
 
-driver.get("https://olympics.com/en/paris-2024/medals")
 
-try:        
-  elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "dataTables_scrollBody")))
-finally:        
-  print('loaded')
+ds=co.datasets.create(name='space', data=open('spaceCompanies.json', 'rb'), keep_fields=['companyName','phone','companyLocation'], dataset_type="embed-input").wait()
 
+
+my_dataset = co.datasets.create(
+	name="space",
+	data=open("spaceCompanies.json", "rb"),
+	dataset_type="prompt-completion-finetune-input")
+
+
+
+
+
+import cohere 
+import pandas as pd 
+cohere_api_key = os.environ["COHERE_API_KEY"]
+MONGO_CONN_STR = os.environ["MONGODB_CONNECTION_STR"]
+co_client = cohere.Client(cohere_api_key, client_name='mongodb')
+dataset = co_client.datasets_create(name='companies',
+                                   data=open('spaceCompanies','r'), 
+                                   dataset_type="embed-input").wait()
+
+dataset.wait()
+dataset
+
+dataset.wait()
+# Schedule an Embedding job to run on the entire movies dataset
+embed_job = co_client.create_embed_job(dataset_id=dataset.id, 
+    input_type='search_document',
+    model='embed-english-v3.0', 
+    truncate='END')
+embed_job.wait()
+output_dataset = co_client.get_dataset(embed_job.output.id)
+results = list(map(lambda x:{"text":x["text"], "embedding": x["embeddings"]["float"]},output_dataset))
+len(results)
+
+
+
+
+
+
+
+
+
+
+
+
+
+co = cohere.Client(api_key='FMGmtPHuF5DeewG7oUANfBGisUlhtlBMIhdRq8R5')
+
+
+
+import pandas as pd
+import s3fs
+df = pd.read_json("s3://ashwin-partner-bucket/cohere/movies_sample_dataset.jsonl", orient="records", lines=True)
+df.to_json('spaceCompanies', orient="records", lines=True)
+df[:3]
+
+df[:3]
+
+df = pandas.read_csv(books.csv, sep='delimiter', header=None)
+df = pd.read_csv('books.csv', encoding='ISO-8859-1')
+
+data = pd.read_csv('books.csv', skiprows=2)
+df = pd.read_csv('books.csv', engine='python')
+
+df = pd.read_csv("books.csv",encoding='utf-16')
