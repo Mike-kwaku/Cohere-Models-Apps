491e79dd35dd5364b2ea9928200f7ce06997a5c3
Mike-kwaku <30466141+Mike-kwaku@users.noreply.github.com>
2024-08-01 04:41:59 -0700

    Create ld.py

---
+++ ld.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://olympics.com/en/paris-2024/medals")
+
+try:        
+  elem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "dataTables_scrollBody")))
+ finally:        
+  print('loaded')
+
